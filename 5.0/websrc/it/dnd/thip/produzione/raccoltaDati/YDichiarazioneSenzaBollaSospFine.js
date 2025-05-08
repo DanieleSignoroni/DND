9fb0ac689f77ad94a87124593ed59769112c236e
Daniele aka COBRA <daniele.signoroni@softre.it>
2025-05-08 09:44:51 +0200

    cleaning

--- 5.0/websrc/it/dnd/thip/produzione/raccoltaDati/YDichiarazioneSenzaBollaSospFine.js
+++
@@ -1,9 +0,0 @@
-var oldRilevDatiPrdTSOL = RilevDatiPrdTSOL;
-
-RilevDatiPrdTSOL = function() {
-	oldRilevDatiPrdTSOL();
-	parent.document.getElementById('YRiposizionamentoUdcBut').style.display = displayNone;
-	parent.document.getElementById('YChiamataUdcBut').style.display = displayNone;
-	parent.document.getElementById('YGeneraUdsAuto').style.display = displayNone; //71946
-	parent.document.getElementById('YGeneraUdsMan').style.display = displayNone; //71946
-}
