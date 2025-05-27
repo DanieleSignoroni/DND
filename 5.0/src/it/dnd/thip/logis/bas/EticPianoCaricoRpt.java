4c4657c103730ea2c344beef338580c2c1d53807
Daniele aka COBRA <daniele.signoroni@softre.it>
2025-05-27 17:04:23 +0200

    stampa etichette

---
+++ 5.0/src/it/dnd/thip/logis/bas/EticPianoCaricoRpt.java
@@ -0,0 +1,40 @@
+package it.dnd.thip.logis.bas;
+
+import java.sql.SQLException;
+
+import com.thera.thermfw.base.Trace;
+import com.thera.thermfw.common.Numerator;
+import com.thera.thermfw.common.NumeratorException;
+import com.thera.thermfw.persist.ErrorCodes;
+
+/**
+ * <p></p>
+ *
+ * <p>
+ * Company: Softre Solutions<br>
+ * Author: Daniele Signoroni<br>
+ * Date: 20/05/2025
+ * </p>
+ */
+
+/*
+ * Revisions:
+ * Number   Date        Owner    Description
+ * 71XXX    20/05/2025  DSSOF3   Prima stesura
+ */
+
+public class EticPianoCaricoRpt extends EticPianoCaricoRptPO {
+
+	public int save() throws SQLException {
+		if (getReportNr() == 0) {
+			try {
+				setReportNr(Numerator.getNextInt("BAS.LR_SOVRACOLLO"));
+			}
+			catch (NumeratorException n) {
+				n.printStackTrace(Trace.excStream);
+				return ErrorCodes.NO_ROWS_UPDATED;
+			}
+		}
+		return super.save();
+	}
+}
